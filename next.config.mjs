aead188ec0a2e3103827b49fc2507f88a039fdac
Yudis <yudis10@users.noreply.github.com>
2023-11-14 07:21:42 +0700

    update packages, config template and meta viewports

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // output: "export",
+  // basePath: "/design/static/starter-project/preview",
+  // distDir: "preview",
 }
 
 export default nextConfig
