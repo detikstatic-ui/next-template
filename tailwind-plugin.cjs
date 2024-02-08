67890bd2bb9d877f65c1efd2fa39d2341cb8aa06
YDST <yudis10@users.noreply.github.com>
2024-02-08 08:56:34 +0700

    Remove modal preset

--- tailwind-plugin.cjs
+++ tailwind-plugin.cjs
@@ -72,77 +72,7 @@ module.exports = plugin(
           {
             transform: "rotateY(0deg)",
           },
-      },
-      ".modal": {
-        "@apply isolate pointer-events-none fixed grid h-full max-h-none w-full max-w-none justify-items-center opacity-0 overscroll-contain z-50 bg-transparent text-inherit duration-200 ease-out transition-all overflow-y-hidden m-0 p-0 inset-0 backdrop:bg-black/30 backdrop:animate-fade backdrop:animate-ease-out backdrop:animate-duration-200 open:pointer-events-auto open:visible open:opacity-100":
-          {},
-        ":root:has(:is(&-open, &:target, &-toggle:checked + &, &[open]))": {
-          overflow: "hidden",
-        },
-        "&-scroll": {
-          overscrollBehavior: "auto",
-        },
-        "&-open, &:target, &-toggle:checked + &, &[open]": {
-          "@apply pointer-events-auto visible opacity-100": {},
-        },
-        "&-action": {
-          "@apply flex justify-end mt-6": {},
-        },
-        "&-toggle": {
-          "@apply fixed h-0 w-0 appearance-none opacity-0": {},
-        },
-        ":where(&)": {
-          "@apply items-center": {},
-        },
-        "&[open] &-box": {
-          "@apply scale-100": {},
-        },
-        "&-box": {
-          "@apply max-h-[calc(100vh_-_5em)] col-start-1 row-start-1 w-11/12 max-w-lg bg-white transition ease-out duration-200 shadow-2xl overflow-y-auto overscroll-contain p-6 scale-90":
-            {},
-        },
-        "&-backdrop": {
-          "@apply -z-[1] col-start-1 row-start-1 grid self-stretch justify-self-stretch text-transparent":
-            {},
-          "& button": {
-            "@apply cursor-default": {},
-          },
-        },
-        "&:not(dialog:not(&-open)), &::backdrop": {
-          backgroundColor: "rgba(0, 0, 0, 0.3)",
-          animation: "modal-pop 0.2s ease-out",
-        },
-        "&-open &-box, &-toggle:checked + & &-box, &:target &-box, &[open] &-box":
-          {
-            "@apply scale-100 translate-y-0": {},
-          },
-        "&-action > :not([hidden]) ~ :not([hidden])": {
-          " --tw-space-x-reverse": "0",
-          marginRight: "calc(0.5rem * var(--tw-space-x-reverse))",
-          marginLeft: "calc(0.5rem * calc(1 - var(--tw-space-x-reverse)))",
-        },
-        "&-top": {
-          "@apply place-items-start": {},
-        },
-        "&-middle": {
-          "@apply place-items-center": {},
-        },
-        "&-bottom": {
-          "@apply place-items-end": {},
-        },
-        "&-top :where(&-box)": {
-          "@apply w-full max-w-none -translate-y-10 scale-100 rounded-t-none rounded-b-2xl":
-            {},
-        },
-        "&-middle :where(&-box)": {
-          "@apply w-11/12 max-w-lg translate-y-0 scale-90 rounded-t-2xl rounded-b-2xl":
-            {},
-        },
-        "&-bottom :where(&-box)": {
-          "@apply w-full max-w-none translate-y-10 scale-100 rounded-b-none rounded-t-2xl":
-            {},
-        },
-      },
+      },      
       ".form-group": {
         "@apply mb-6": {},
         "&-inline": {
